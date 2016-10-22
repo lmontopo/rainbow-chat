66939c3c3f0610715f7754fd6bf3fbdea9593ce9
Leta Montopoli <leta@securitycompass.com>
2016-10-22 16:06:01 -0400

    Add flask app + db interaction.

---
+++ backend/app/main/routes.py
@@ -0,0 +1,25 @@
+from flask import session, redirect, url_for, render_template, request
+from . import main
+from .forms import LoginForm
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    """"Login form to enter a room."""
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['name'] = 'Anonymous'
+        session['room'] = 1
+        return redirect(url_for('.chat'))
+    return render_template('index.html', form=form)
+
+
+@main.route('/chat')
+def chat():
+    """Chat room. The user's name and room must be stored in
+    the session."""
+    name = session.get('name', '')
+    room = session.get('room', '')
+    if name == '' or room == '':
+        return redirect(url_for('.index'))
+    return render_template('chat.html', name=name, room=room)
